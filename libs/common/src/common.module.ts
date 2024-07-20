570b0b61d5bf7b5c9790aea56d80ab4985df596f
OhYongUn <dhdyddjs@naver.com>
2024-07-20 17:50:10 +0900

    db 커넥션 공통모듈 분리

--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -1,8 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonService } from './common.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
+@Global()
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env-${process.env.NODE_ENV}`, // 환경에 맞는 .env 파일 지정
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DATABASE_HOST'),
+        port: config.get<number>('DATABASE_PORT'),
+        username: config.get<string>('DATABASE_USERNAME'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        database: config.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: config.get<boolean>('DATABASE_SYNCHRONIZE'),
+      }),
+    }),
+  ],
   providers: [CommonService],
-  exports: [CommonService],
+  exports: [CommonService, TypeOrmModule],
 })
 export class CommonModule {}
