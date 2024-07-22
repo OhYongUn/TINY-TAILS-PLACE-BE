098e971633b5c2538a46e22e876648c16c127306
OhYongUn <dhdyddjs@naver.com>
2024-07-22 21:51:30 +0900

    포트변경

--- apps/rest-api/src/main.ts
+++ apps/rest-api/src/main.ts
@@ -4,7 +4,7 @@ import { RestApiModule } from './rest-api.module';
 async function bootstrap() {
   const app = await NestFactory.create(RestApiModule);
   app.enableCors(); // CORS 설정이 필요한 경우
-  await app.listen(3000);
+  await app.listen(4000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
