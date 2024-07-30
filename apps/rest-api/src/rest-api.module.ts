1299b28c6565b69dfb59e3552a51f902e53d763b
OhYongUn <dhdyddjs@naver.com>
2024-07-30 21:07:46 +0900

    jwt 인증 기능 추가

--- apps/rest-api/src/rest-api.module.ts
+++ apps/rest-api/src/rest-api.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from '@app/common/common.module';
-import { AuthModule } from '@app/common/auth/auth.module'; // CommonModule의 경로에 따라 조정 필요
-import { AuthController } from './auth/auth.controller';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CommonModule, AuthModule, UsersModule],
-  controllers: [AuthController, UsersController],
+  imports: [CommonModule, UsersModule],
+  controllers: [UsersController],
   providers: [],
 })
 export class RestApiModule {}
