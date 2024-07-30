f69871953b65358310dd3c3b31d218df5ab274b3
OhYongUn <dhdyddjs@naver.com>
2024-07-30 21:36:47 +0900

    회원가입기능 추가

--- libs/common/src/users/users.service.ts
+++ libs/common/src/users/users.service.ts
@@ -1,10 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import * as bcryptjs from 'bcryptjs';
+import { CreateUserRequest } from './dto/create-user.request';
+import { PrismaService } from '../prisma/prisma.service';
 import { Prisma } from '@prisma/client';
-import { PrismaService } from '@app/common/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: CreateUserRequest) {
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          name: data.name,
+          email: data.email,
+          password: await bcryptjs.hash(data.password, 10),
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      });
+    } catch (err) {
+      if (err.code === 'P2002') {
+        throw new UnprocessableEntityException('Email already exists.');
+      }
+      throw err;
+    }
+  }
 
   async getUser(filter: Prisma.UserWhereUniqueInput) {
     return this.prismaService.user.findUniqueOrThrow({
