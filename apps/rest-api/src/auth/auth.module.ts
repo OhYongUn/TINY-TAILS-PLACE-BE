b1377e8e9a289d739d7c465fa89b3dec32ceaa0b
OhYongUn <dhdyddjs@naver.com>
2024-07-30 21:07:10 +0900

    auth 모듈 수정

--- apps/rest-api/src/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from '@app/common/auth/auth.service'; // 공통 모듈에서 AuthService 임포트
-
-@Module({
-  controllers: [AuthController], // 컨트롤러 등록
-  providers: [AuthService], // 서비스 등록
-})
-export class AuthModule {}
--- libs/common/src/auth/auth.module.ts
+++ libs/common/src/auth/auth.module.ts
@@ -1,26 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
-import { UserModule } from '../user/user.module';
-import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
-    UserModule,
-    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '15m' },
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.getOrThrow('JWT_EXPIRATION'),
+        },
       }),
+      inject: [ConfigService],
     }),
     ConfigModule,
+    UsersModule,
   ],
-  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
   exports: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
