eb597f3a76cece6f251d068356d7fa4d6f13a8ac
OhYongUn <ymjyongun@gmail.com>
2024-08-06 13:11:44 +0900

    SwaggerModule을 추가한다.

--- apps/rest-api/src/main.ts
+++ apps/rest-api/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { RestApiModule } from './rest-api.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(RestApiModule);
   app.enableCors(); // CORS 설정이 필요한 경우
+
+  const config = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(4000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
