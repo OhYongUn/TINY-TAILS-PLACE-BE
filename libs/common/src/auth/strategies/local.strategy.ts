2a55379ab6b061c6c595c451a187c0bf2a0d5ed5
OhYongUn <ymjyongun@gmail.com>
2024-08-20 16:25:16 +0900

    관리자 인증  local.strategy 추가

--- libs/common/src/auth/strategies/local.strategy.ts
+++ libs/common/src/auth/strategies/local.strategy.ts
@@ -1,18 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-local';
-import { AuthService } from '../auth.service';
+import {Injectable, UnauthorizedException} from '@nestjs/common';
+import {PassportStrategy} from '@nestjs/passport';
+import {Strategy} from 'passport-local';
+import {AuthService} from '../auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
-    super({
-      usernameField: 'email',
-      passwordField: 'password',
-    });
-  }
+    constructor(private readonly authService: AuthService) {
+        super({
+            usernameField: 'email',
+            passwordField: 'password',
+            passReqToCallback: true,
 
-  async validate(email: string, password: string) {
-    return await this.authService.validateUser(email, password);
-  }
+        });
+    }
+
+    async validate(req: any, email: string, password: string): Promise<any> {
+        const isAdminRoute = req.route.path.includes('/admin');
+        let user;
+
+        try {
+            if (isAdminRoute) {
+                user = await this.authService.validateAdmin(email, password);
+            } else {
+                user = await this.authService.validateUser(email, password);
+            }
+        } catch (error) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+        return user;
+
+    }
 }
