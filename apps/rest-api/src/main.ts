25aef760c68c9d18239de28112e2493df07620e1
OhYongUn <dhdyddjs@naver.com>
2024-08-21 22:01:32 +0900

    esllint오류수정

--- apps/rest-api/src/main.ts
+++ apps/rest-api/src/main.ts
@@ -1,36 +1,34 @@
-import {HttpAdapterHost, NestFactory} from '@nestjs/core';
-import {RestApiModule} from './rest-api.module';
-import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
-import {ValidationPipe} from '@nestjs/common';
-import {GlobalExceptionFilter} from "@app/common/exceptions/global-exception.filter";
-import {SuccessResponseInterceptor} from "@app/common/interceptors/success-response.interceptor";
+import { NestFactory } from '@nestjs/core';
+import { RestApiModule } from './rest-api.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { SuccessResponseInterceptor } from '@app/common/interceptors/success-response.interceptor';
 
 async function bootstrap() {
-    const app = await NestFactory.create(RestApiModule);
-    app.enableCors(); // CORS 설정이 필요한 경우
+  const app = await NestFactory.create(RestApiModule);
+  app.enableCors(); // CORS 설정이 필요한 경우
 
-    const config = new DocumentBuilder()
-        .setTitle('Your API Title')
-        .setDescription('API description')
-        .setVersion('1.0')
-        .build();
-    app.useGlobalPipes(
-        new ValidationPipe({
-            transform: true,
-            whitelist: true,
-            forbidNonWhitelisted: true,
-        }),
-    );
-    /*
+  const config = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
-    const httpAdapterHost = app.get(HttpAdapterHost);
-    app.useGlobalFilters(new GlobalExceptionFilter(httpAdapterHost));
-    app.useGlobalInterceptors(new SuccessResponseInterceptor());*/
+  //const httpAdapterHost = app.get(HttpAdapterHost);
+  //app.useGlobalFilters(new GlobalExceptionFilter(httpAdapterHost));
+  app.useGlobalInterceptors(new SuccessResponseInterceptor());
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
-    await app.listen(4000);
-    console.log(`Application is running on: ${await app.getUrl()}`);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(4000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
--- libs/common/src/interceptors/success-response.interceptor.ts
+++ libs/common/src/interceptors/success-response.interceptor.ts
@@ -1,48 +1,58 @@
 // common/src/interceptors/success-response.interceptor.ts
 
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, HttpStatus } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export interface CustomResponse<T> {
-    data: T;
-    message?: string;
-    statusCode?: number;
+  data: T;
+  message?: string;
+  statusCode?: number;
 }
 
 export interface SuccessResponse<T> {
-    success: boolean;
-    statusCode: number;
-    data: T;
-    message?: string;
-    error: null;
+  success: boolean;
+  statusCode: number;
+  data: T;
+  message?: string;
+  error: null;
 }
 
 @Injectable()
-export class SuccessResponseInterceptor<T> implements NestInterceptor<T, SuccessResponse<T>> {
-    intercept(context: ExecutionContext, next: CallHandler): Observable<SuccessResponse<T>> {
-        return next.handle().pipe(
-            map(response => {
-                const statusCode = context.switchToHttp().getResponse().statusCode;
+export class SuccessResponseInterceptor<T>
+  implements NestInterceptor<T, SuccessResponse<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<SuccessResponse<T>> {
+    return next.handle().pipe(
+      map((response) => {
+        const statusCode = context.switchToHttp().getResponse().statusCode;
 
-                if (response && typeof response === 'object' && 'data' in response) {
-                    const customResponse = response as CustomResponse<T>;
-                    return {
-                        success: true,
-                        statusCode: customResponse.statusCode || statusCode,
-                        data: customResponse.data,
-                        message: customResponse.message,
-                        error: null,
-                    };
-                }
+        if (response && typeof response === 'object' && 'data' in response) {
+          const customResponse = response as CustomResponse<T>;
+          return {
+            success: true,
+            statusCode: customResponse.statusCode || statusCode,
+            data: customResponse.data,
+            message: customResponse.message,
+            error: null,
+          };
+        }
 
-                return {
-                    success: true,
-                    statusCode,
-                    data: response,
-                    error: null,
-                };
-            }),
-        );
-    }
+        return {
+          success: true,
+          statusCode,
+          data: response,
+          error: null,
+        };
+      }),
+    );
+  }
 }
